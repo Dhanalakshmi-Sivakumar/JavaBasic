ea37dc95bc20f6f8d4eb8c17c7d50df6d75bfb3b
Asus <Asus@X515EA>
2024-06-27 11:48:15 +0530

    This is my char demo project

---
+++ javabasic/src/day1/chardemo.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class chardemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- javabasic/src/day1/sample1.java
+++ javabasic/src/day1/sample1.java
@@ -3,7 +3,9 @@ package day1;
 public class sample1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to JFS");
+		System.out.println("MVIT");
+		System.out.print("TNSIF);
 
 	}
 
